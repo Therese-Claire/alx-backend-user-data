cefadea984995e33406d14bf04979ba314a1ca6b
Therese-Claire <awonguagabi@gmail.com>
2024-08-03 01:05:10 +0100

    edited task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" Use of regex in replacing occurrences of certain field values """
+"""defines a function filter_datum"""
+import os
 import re
-from typing import List
 import logging
 import mysql.connector
-import os
+from typing import List
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -18,54 +18,45 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 def get_logger() -> logging.Logger:
-    """ Returns a logging.Logger object """
-    logger = logging.getLogger("user_data")
+    """Creates and returns a new logger for user data
+
+    Returns:
+        A new logger object
+    """
+    logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
-
-    target_handler = logging.StreamHandler()
-    target_handler.setLevel(logging.INFO)
-
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handle.setFormatter(formatter)
-
-    logger.addHandler(target_handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
     return logger
 
 
-def get_db() -> mysql.connector.connection.MYSQLConnection:
-    """ Connection to MySQL environment
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Creates a connection to a secure mysql database
 
     Returns:
         A MySQLConnection object that was created.
     """
-    db_connect = {
+    credentials = {
         'user': os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         'password': os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         'host': os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
         'database': os.getenv('PERSONAL_DATA_DB_NAME')
         }
-    return mysql.connector.connect(**db_connect)
+    return mysql.connector.connect(**credentials)
 
 
-def main() -> None:
-    """ Obtain database connection using get_db
-    retrieve all role in the users table and display
-    each row under a filtered format
-    """
+def main():
+    """ main function for retrieving user data and log it to the console"""
     db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-
-    headers = [field[0] for field in cursor.description]
     logger = get_logger()
-
-    for row in cursor:
-        info_answer = ''
-        for f, p in zip(row, headers):
-            info_answer += f'{p}={(f)}; '
-        logger.info(info_answer)
-
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for row in rows:
+        row_str = "; ".join(f"{key}={value}" for key, value in row.items())
+        logger.info(row_str.strip())
     cursor.close()
     db.close()
 
@@ -83,9 +74,13 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ Returns filtered values from log records """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+        """formats the logged message to be redacted
+        using filter_datum fuction.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
 
 if __name__ == '__main__':
     main()
